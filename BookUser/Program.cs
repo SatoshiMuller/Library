d2f8a36bdc1609b9f872fb157b9f866c9c43dcd5
Satoshi Muller <ilya.vasiliev0@yandex.ru>
2023-09-14 08:30:06 +0300

    Test commit

--- BookUser/Program.cs
+++ BookUser/Program.cs
@@ -14,6 +14,7 @@ namespace BookUser
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Main());
+
         }
     }
 }
